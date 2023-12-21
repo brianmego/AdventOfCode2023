a514b987736001331b1fb59d5fc38a2e55dfd50c
Brian Mego <brianmego@gmail.com>
2023-12-20 23:17:39 -0600

    Day20 refactor partial

--- day-20/src/bin/shared/mod.rs
+++ day-20/src/bin/shared/mod.rs
@@ -7,6 +7,7 @@ use nom::{
     sequence::{delimited, terminated, tuple},
     IResult,
 };
+use std::fmt::Debug;
 use std::collections::VecDeque;
 
 #[derive(Debug, PartialEq)]
@@ -24,75 +25,164 @@ enum ModuleType {
 }
 
 #[derive(Debug, PartialEq)]
-struct Module<'a> {
-    module_type: ModuleType,
-    name: &'a str,
-    current_state: Signal,
+enum FlipFlopState {
+    On,
+    Off,
+}
+#[derive(Debug, PartialEq)]
+struct FlipFlop {
+    name: String,
+    current_state: FlipFlopState,
     commands: VecDeque<Signal>,
-    destination_modules: Vec<&'a str>,
+    destination_modules: Vec<String>,
 }
-impl<'a> Module<'a> {
-    fn new(module_type: ModuleType, name: &'a str, destination_modules: Vec<&'a str>) -> Self {
-        let current_state = Signal::Low;
-        let commands = VecDeque::new();
+
+impl FlipFlop {
+    fn new(name: &str, destination_modules: Vec<&str>) -> Self {
         Self {
-            module_type,
-            name,
-            current_state,
-            commands,
-            destination_modules,
+            name: name.to_string(),
+            current_state: FlipFlopState::Off,
+            commands: VecDeque::new(),
+            destination_modules: destination_modules.iter().map(|s| s.to_string()).collect(),
         }
     }
+}
+#[derive(Default, Debug, PartialEq)]
+struct Broadcaster {
+    destination_modules: Vec<String>,
+}
 
-    fn handle_next_command(&mut self) {
-        match self.current_state {
-            Signal::High => match self.commands.pop_front().unwrap() {
-                Signal::High => todo!(),
-                Signal::Low => todo!(),
-            },
-            Signal::Low => match self.commands.pop_front().unwrap() {
-                Signal::High => todo!(),
-                Signal::Low => todo!(),
-            },
+impl Broadcaster {
+    fn new(destination_modules: Vec<&str>) -> Self {
+        Self {
+            destination_modules: destination_modules.iter().map(|s| s.to_string()).collect(),
+        }
+    }
+}
+#[derive(Debug, PartialEq)]
+struct Conjunction {
+    name: String,
+    commands: VecDeque<Signal>,
+    last_command: Option<Signal>,
+    destination_modules: Vec<String>,
+}
+
+impl Conjunction {
+    fn new(name: &str, destination_modules: Vec<&str>) -> Self {
+        Self {
+            name: name.to_string(),
+            commands: VecDeque::new(),
+            last_command: None,
+            destination_modules: destination_modules.iter().map(|s| s.to_string()).collect(),
         }
     }
 }
 
-fn parse_broadcaster(inp: &str) -> IResult<&str, Module> {
+trait Module {
+    fn queue_command(&mut self, signal: Signal) {
+        self.commands().push_back(signal);
+    }
+    fn commands(&mut self) -> VecDeque<Signal>;
+}
+impl Module for FlipFlop {
+    fn commands(&mut self) -> VecDeque<Signal> {
+        self.commands
+    }
+}
+impl Module for Conjunction {
+    fn commands(&mut self) -> VecDeque<Signal> {
+        self.commands
+    }
+}
+impl Module for Broadcaster {
+    fn commands(&mut self) -> VecDeque<Signal> {
+        todo!()
+    }
+}
+// #[derive(Debug, PartialEq)]
+// struct Module<'a> {
+//     module_type: ModuleType,
+//     name: &'a str,
+//     current_state: Signal,
+//     commands: VecDeque<Signal>,
+//     destination_modules: Vec<&'a str>,
+// }
+// impl<'a> Module<'a> {
+//     fn new(module_type: ModuleType, name: &'a str, destination_modules: Vec<&'a str>) -> Self {
+//         let current_state = Signal::Low;
+//         let commands = VecDeque::new();
+//         Self {
+//             module_type,
+//             name,
+//             current_state,
+//             commands,
+//             destination_modules,
+//         }
+//     }
+
+//     fn queue_command(&mut self, signal: Signal) {
+//         self.commands.push_back(signal);
+//     }
+
+//     fn handle_next_command(&mut self, mut modules: Vec<Module>) {
+//         match self.module_type {
+//             ModuleType::Broadcast => {
+//                 self.destination_modules.iter().for_each(|name| {
+//                     let target_module = modules.iter_mut().find(|m|&m.name == name).unwrap();
+//                     target_module.queue_command(Signal::Low);
+//                 })
+//             },
+//             ModuleType::FlipFlop => {
+
+//             },
+//             ModuleType::Conjunction => todo!(),
+//             ModuleType::Button => todo!(),
+//         }
+//         match self.current_state {
+//             Signal::High => match self.commands.pop_front().unwrap() {
+//                 Signal::High => todo!(),
+//                 Signal::Low => todo!(),
+//             },
+//             Signal::Low => match self.commands.pop_front().unwrap() {
+//                 Signal::High => todo!(),
+//                 Signal::Low => todo!(),
+//             },
+//         }
+//     }
+// }
+
+fn parse_broadcaster(inp: &str) -> IResult<&str, impl Module + Debug + PartialEq> {
     map(
         tuple((
             terminated(tag("broadcaster"), tag(" -> ")),
             many1(terminated(alpha1, opt(tag(", ")))),
         )),
-        |(name, destination_modules)| Module::new(ModuleType::Broadcast, name, destination_modules),
+        |(name, destination_modules)| Broadcaster::default(),
     )(inp)
 }
-fn parse_flip_flop(inp: &str) -> IResult<&str, Module> {
+fn parse_flip_flop(inp: &str) -> IResult<&str, impl Module + Debug + PartialEq> {
     map(
         tuple((
             delimited(nomchar('%'), alpha1, tag(" -> ")),
             many1(terminated(alpha1, opt(tag(", ")))),
         )),
-        |(name, destination_modules)| Module::new(ModuleType::FlipFlop, name, destination_modules),
+        |(name, destination_modules): (&str, Vec<&str>)| FlipFlop::new(name, destination_modules),
     )(inp)
 }
-fn parse_conjunction(inp: &str) -> IResult<&str, Module> {
+fn parse_conjunction(inp: &str) -> IResult<&str, impl Module + Debug + PartialEq> {
     map(
         tuple((
             delimited(nomchar('&'), alpha1, tag(" -> ")),
             many1(terminated(alpha1, opt(tag(", ")))),
         )),
-        |(name, destination_modules)| Module::new(ModuleType::Conjunction, name, destination_modules),
+        |(name, destination_modules)| Conjunction::new(name, destination_modules),
     )(inp)
 }
-pub fn parse_configuration(inp: &str) -> IResult<&str, Vec<Module>> {
-    many1(
-        terminated(alt((
-            parse_broadcaster,
-            parse_flip_flop,
-            parse_conjunction,
-        )), newline)
-    )(inp)
+pub fn parse_configuration(inp: &str) -> IResult<&str, Vec<impl Module>> {
+    many1(terminated(
+        alt((parse_broadcaster, parse_flip_flop, parse_conjunction)),
+        newline,
+    ))(inp)
 }
 
 #[cfg(test)]
@@ -104,23 +194,20 @@ mod tests {
     fn test_parse_broadcaster() {
         let inp = "broadcaster -> a, b, c";
         let actual = parse_broadcaster(inp);
-        let expected = Ok((
-            "",
-            Module::new(ModuleType::Broadcast, "broadcaster", vec!["a", "b", "c"]),
-        ));
+        let expected = Ok(("", Broadcaster::new(vec!["a", "b", "c"])));
         assert_eq!(actual, expected);
     }
 
-    #[test_case("%a -> b", Ok(("", Module::new(ModuleType::FlipFlop, "a", vec!["b"]))))]
-    #[test_case("%b -> c", Ok(("", Module::new(ModuleType::FlipFlop, "b", vec!["c"]))))]
-    #[test_case("%c -> inv", Ok(("", Module::new(ModuleType::FlipFlop, "c", vec!["inv"]))))]
-    fn test_flip_flop(inp: &str, exp: IResult<&str, Module>) {
+    #[test_case("%a -> b", Ok(("", FlipFlop::new("a", vec!["b"]))))]
+    #[test_case("%b -> c", Ok(("", FlipFlop::new("b", vec!["c"]))))]
+    #[test_case("%c -> inv", Ok(("", FlipFlop::new("c", vec!["inv"]))))]
+    fn test_flip_flop(inp: &str, exp: IResult<&str, FlipFlop>) {
         let actual = parse_flip_flop(inp);
         assert_eq!(actual, exp);
     }
 
-    #[test_case("&inv -> a", Ok(("", Module::new(ModuleType::Conjunction, "inv", vec!["a"]))))]
-    fn test_conjunction(inp: &str, exp: IResult<&str, Module>) {
+    #[test_case("&inv -> a", Ok(("", Conjunction::new("inv", vec!["a"]))))]
+    fn test_conjunction(inp: &str, exp: IResult<&str, Conjunction>) {
         let actual = parse_conjunction(inp);
         assert_eq!(actual, exp);
     }
